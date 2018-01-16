3b271f807be54ad26de313f8413a76e9a72e1a68
Julia Morgan <juliamorgan@legalshieldcorp.com>
2018-01-16 09:51:59 -0600

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20180112222210) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "miscroposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
